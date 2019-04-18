a1665be6b51161cfff9a8b5765939320d2db112a
davi ribeiro <luminuszz43@gmail.com>
2019-04-17 21:27:55 -0300

    Introdução ao java script

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+// Programa java Script introdução//
+var a = 5;
+var b = "ola mundo";
+
+var soma = a+b;
+var sub = a-b; 
+var div = a/b;
+var mul = a*b;
+
+// Estruttura IF
+if (a*b == 6) {
+    alert("Numero impar");
+}
+// Estrutura else if
+else if (a*b == 4){ 
+    alert("Numero par!");
+}
+//Estrutura Else 
+else {
+    alert("Informe um valor valido");
+}
+
+//Estrutura de repetição While
+var i = 0
+while (i<3){
+ alert(i);
+ i=i+1;
+}
+
+// Estrutura de repetição For
+for (var d = 0; d < 3; d++ ){
+    alert(d);
+}
+
+// tipos variaveis 
+
+var Numero = 5;
+var decimal = 6.5;
+var texto = "Ola mundo";
+var array = ["davi","tina","laranja", "maçã"]
+
+// chamando arrays
+alert(array);
+
+// Usando for
+for(i in array){
+    alert(array[i]);
+}
+
+// Utilizando console,log
+
+console.log("EAI!!!");
+
+//Introdução a Funções
+
---
+++ script2.js
@@ -0,0 +1,4 @@
+function davi(a, b){
+console.log (a+b);
+}
+davi(1,2);
