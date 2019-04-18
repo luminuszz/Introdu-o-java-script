e7e7d1e680ce60f654222e80d159f979c716bbc9
davi ribeiro <luminuszz43@gmail.com>
2019-04-18 17:26:33 -0300

    Finalizando JavaScript

--- script2.js
+++ script2.js
@@ -18,6 +18,31 @@ return a*b;
 console.log(d4(5,5));
 */
 
-function msg(){
-alert("Não clique em mim");
+// Usando onclick
+ function msg(nome){
+alert("Não clique em mim"+nome );
+ }
+
+// Usando mouseover
+function mensagem(nomee){
+alert("não aproxime o mouse do botão" + nomee);
+
+}
+
+// Usando DOM
+function alteracor1(cor){
+    var elemento = document.getElementById("mensagem_span");
+    elemento.style.backgroundColor = cor;
+}
+
+// Validação Simples com form
+function valida(){
+    var nome = document.getElementById('nome');
+   if(nome.value == ""){
+    alert("Informe seu nome")
+   }
+   else{
+       alert("Enviado com sucesso");
+   }
+
 }
