9ad4c8478f471260cbd1baf8696d6d19493484c9
davi ribeiro <luminuszz43@gmail.com>
2019-04-17 21:39:50 -0300

    Continuação de funções

--- script2.js
+++ script2.js
@@ -1,4 +1,19 @@
+// Funções
 function davi(a, b){
 console.log (a+b);
 }
 davi(1,2);
+
+// Chamando funções utilizando return
+function d3(a,b){
+return a-b;
+}
+var s = d3(5,3);
+console.log(s);
+
+function d4(a,b){
+return a*b;
+}
+
+console.log(d4(5,5));
+
