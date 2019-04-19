ce4aedee8c6363a074c24f12eeb24b3600fbfd43
davi ribeiro <luminuszz43@gmail.com>
2019-04-18 21:21:54 -0300

    Continuação ao Jquery, Utilizando de funções e slide show

--- script_jquery.js
+++ script_jquery.js
@@ -19,13 +19,54 @@ $(function(){
 */
 
 // alterando css parte 2
-$(function(){
+/*$(function(){
     $('#azul').click(function(){
         $('p').css("color", "blue");
     });
 
     $('#vermelho').click(function(){
         $('p').css("background-color","red");
+
+    });
+
+});
+*/
+
+//Efeito Fade out obs: fade in: reaparece o conteudo (pode-se usar parametros "slow", "fast" e numericos)
+/*$(function(){
+    $('#azul').click(function(){
+        $('p').css("background-color","blue");
+        $('p').fadeOut();
+        $('p').delay(3000);
+        $('p').fadeIn('slow')
+    });
+
+});
+*/
+
+// Gravar mensagem usando java Script utlizando o .text (obs pode-se customizar essa mensagem com css)
+// Pode-se controlar o tempo da messagem usando .fadeOut.
+//Obs técnica de encadamaento.
+//Declaração multiplica de CSS utilizando chaves {}.
+$(function(){
+    $('#azul').click(function(){
+        $('p').css("background-color","blue")
+        $('#msg1')
+            //Encadeamento em bloco
+            .text("Cor alterada com sucesso!!")
+            .css("color","red").css("border","1px solid red")
+            .fadeOut(3000)
+            //Declaração multipla usando chaves.
+            .css({margin: "50px", marginTop: "80px;", backgroundColor: "blue" });
+    });
+
+});
+
+// Slide show usando Jquery.
+
+$(function(){
+    $('#l2').click(function(){
+        $('#i2').show();
 
     });
 
